e3db9043ec884f5abdac6033c5f18a9a75f5749e
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-04-25 22:37:57 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from duckduckgo_search import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="duckduckgo",
+    version="__version__",
+    author="deedy5",
+    author_email="deedy-ru@ya.ru",
+    description="Duckduckgo.com search results",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deedy5/duckduckgo_search",
+    license="MIT",
+    py_modules=["duckduckgo_search"],
+    install_requires=["requests>=2.25.0", "lxml>=4.6.3"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: Implementation :: CPython",  
+        "Topic :: Internet :: WWW/HTTP :: Indexing/Search",
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    python_requires=">=3.6",
+    zip_safe=False,
+)
