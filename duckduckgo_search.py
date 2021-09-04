148a0d054df4b565133e9487340e1e5bff1be4ca
deedy5 <65482418+deedy5@users.noreply.github.com>
2021-09-04 13:02:03 +0300

    v0.6
    Code refactoring

--- duckduckgo_search.py
+++ duckduckgo_search.py
@@ -10,12 +10,10 @@ def ddg(keywords, region='wt-wt', safesearch='Moderate', time=None, max_results=
     keywords: keywords for query;
     safesearch: On (kp = 1), Moderate (kp = -1), Off (kp = -2);
     region: country of results - wt-wt (Global), us-en, uk-en, ru-ru, etc.;
-    time: 'd' (day), 'w' (week), 'm' (month), 'y' (year), or 'year-month-date..year-month-date';
-    max_results: depends on the keyword, the maximum DDG gives out about 200 results.
-    '''
-
-    results, counter = [], 0
-    url = 'https://html.duckduckgo.com/html'    
+    time: 'd' (day), 'w' (week), 'm' (month), 'y' (year), or 'year-month-date..year-month-date';    
+    max_results = 30 gives a number of results not less than 30,   
+    maximum DDG gives out about 200 results.
+    '''    
     safesearch_base = {
         'On': 1, 
         'Moderate': -1, 
@@ -27,26 +25,21 @@ def ddg(keywords, region='wt-wt', safesearch='Moderate', time=None, max_results=
         'p': safesearch_base[safesearch],
         'df': time
         }
+    
     with requests.Session() as s:
         s.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0"})
-        
+        results = []     
         while True:
-            print('i')
-            res = s.post(url, data=payload, **kwargs)
+            res = s.post('https://html.duckduckgo.com/html', data=payload, **kwargs)
             tree = html.fromstring(res.text)
             if tree.xpath('//div[@class="no-results"]/text()'):
-                print(len(results))
                 return results
             for element in tree.xpath('//div[contains(@class, "results_links")]'):
-                position = {
-                    'title': element.xpath('.//a[contains(@class, "result__a")]/text()')[0],
-                    'href': element.xpath('.//a[contains(@class, "result__a")]/@href')[0],
-                    'body': ''.join(element.xpath('.//a[contains(@class, "result__snippet")]//text()')),
-                    }
-                results.append(position)
-                counter += 1
-                if counter >= max_results:
-                    return results
+                results.append({'title': element.xpath('.//a[contains(@class, "result__a")]/text()')[0],
+                                'href': element.xpath('.//a[contains(@class, "result__a")]/@href')[0],
+                                'body': ''.join(element.xpath('.//a[contains(@class, "result__snippet")]//text()')),})
+            if len(results) >= max_results:
+                return results
 
             next_page = tree.xpath('.//div[@class="nav-link"]')[-1] 
             names = next_page.xpath('.//input[@type="hidden"]/@name')
