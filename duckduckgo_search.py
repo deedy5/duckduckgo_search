0bf447412b0b08ddfc68f6376cd397a3689832d8
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-01-20 11:16:32 +0300

    v0.9
    ddg_images() image search function has been added

--- duckduckgo_search.py
+++ duckduckgo_search.py
@@ -1,11 +1,15 @@
+import json
 from time import sleep
-from lxml import html
 import requests
+from lxml import html
 
-__version__ = 0.8
+
+__version__ = 0.9
+
 
 session = requests.Session()
 session.headers.update({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; rv:91.0) Gecko/20100101 Firefox/91.0"})
+
 
 def ddg(keywords, region='wt-wt', safesearch='Moderate', time=None, max_results=30, **kwargs):
     ''' DuckDuckGo search
@@ -48,3 +52,62 @@ def ddg(keywords, region='wt-wt', safesearch='Moderate', time=None, max_results=
         values = next_page.xpath('.//input[@type="hidden"]/@value')
         payload = {n: v for n, v in zip(names, values)}
         sleep(2)
+
+
+        
+def ddg_images(keywords, region='wt-wt', safesearch='Moderate', time=None, size=None,
+               color=None, type_image=None, layout=None, license_image=None, max_results=100):
+    ''' DuckDuckGo images search
+    keywords: keywords for query;
+    safesearch: On (kp = 1), Moderate (kp = -1), Off (kp = -2);
+    region: country of results - wt-wt (Global), us-en, uk-en, ru-ru, etc.;
+    time: Day, Week, Month, Year;
+    size: Small, Medium, Large, Wallpaper;
+    color: color, Monochrome, Red, Orange, Yellow, Green, Blue, Purple, Pink, Brown, Black, Gray, Teal, White;
+    type_image: photo, clipart, gif, transparent, line;
+    layout: Square, Tall, Wide;
+    license_image: any (All Creative Commons), Public (Public Domain), Share (Free to Share and Use),
+             ShareCommercially (Free to Share and Use Commercially), Modify (Free to Modify, Share, and Use),
+             ModifyCommercially (Free to Modify, Share, and Use Commercially);
+    max_results: number of results, maximum ddg_images gives out 1000 results.
+    '''
+    
+    # get vqd
+    payload = {
+        'q': keywords, 
+        }    
+    res = session.post("https://duckduckgo.com", data=payload)
+    tree = html.fromstring(res.text)
+    vqd = tree.xpath("//script[contains(text(), 'vqd=')]/text()")[0].split("vqd='")[-1].split("';")[0]
+
+    # get images
+    safesearch_base = {
+        'On': 1, 
+        'Moderate': -1, 
+        'Off': -2
+        }
+
+    time = f"time:{time}" if time else ''
+    size = f"size:{size}" if size else ''
+    color = f"color:{color}" if color else ''
+    type_image = f"type:{type_image}" if type_image else ''
+    layout = f"layout:{layout}" if layout else ''
+    license_image = f"license:{license_image}" if license_image else ''    
+    payload = { 
+        'l': region,
+        'o': 'json',
+        's': 0,
+        'q': keywords,
+        'vqd': vqd,
+        'f': f"{time},{size},{color},{type_image},{layout},{license_image}",
+        'p': safesearch_base[safesearch]
+        }
+
+    results = []     
+    while payload['s'] < max_results:
+        res = session.get("https://duckduckgo.com/i.js", params=payload)
+        data = res.json()
+        results.extend(r for r in data['results'])
+        payload['s'] += 100
+    return results
+    
