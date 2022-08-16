d9ad7f0347ae42b311362164d7b625a3baed11fa
deedy5 <65482418+deedy5@users.noreply.github.com>
2022-08-16 10:10:05 +0000

    add tests

---
+++ tests/test_duckduckgo_search.py
@@ -0,0 +1,26 @@
+from duckduckgo_search import ddg, ddg_images, ddg_news, ddg_videos, ddg_maps
+
+
+def test_ddg():
+    results = ddg('cat', max_results=100)
+    assert len(results) == 100
+
+
+def test_ddg_images():
+    results = ddg_images('cat', max_results=100)
+    assert len(results) == 100
+
+
+def test_ddg_news():
+    results = ddg_news('cat', max_results=100)
+    assert len(results) == 100
+
+
+def test_ddg_videos():
+    results = ddg_videos('cat', max_results=100)
+    assert len(results) == 100
+
+
+def test_ddg_maps():
+    results = ddg_maps('cat', place='Europe', max_results=100)
+    assert len(results) == 100
