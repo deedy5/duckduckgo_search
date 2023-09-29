aa41a57a2d0413b11e97af879b01062bb8c9de94
deedy5 <65482418+deedy5@users.noreply.github.com>
2023-09-30 01:58:47 +0300

    add tests for AsyncDDGS, improve tests DDGS

---
+++ tests/test_cli.py
@@ -0,0 +1,147 @@
+import os
+import shutil
+from itertools import islice
+
+from click.testing import CliRunner
+
+from duckduckgo_search import DDGS, __version__
+from duckduckgo_search.cli import cli, download_results, save_csv, save_json
+
+runner = CliRunner()
+
+
+def test_version_command():
+    result = runner.invoke(cli, ["version"])
+    assert result.output.strip() == __version__
+
+
+def test_text_command():
+    result = runner.invoke(cli, ["text", "-k", "python"])
+    assert "title" in result.output
+
+
+def test_images_command():
+    result = runner.invoke(cli, ["images", "-k", "cat"])
+    assert "title" in result.output
+
+
+def test_news_command():
+    result = runner.invoke(cli, ["news", "-k", "usa"])
+    assert "title" in result.output
+
+
+def test_videos_command():
+    result = runner.invoke(cli, ["videos", "-k", "dog"])
+    assert "title" in result.output
+
+
+def test_maps_command():
+    result = runner.invoke(cli, ["maps", "-k", "school", "-p", "Berlin"])
+    assert "title" in result.output
+
+
+def test_answers_command():
+    result = runner.invoke(cli, ["answers", "-k", "question"])
+    assert "question" in result.output
+
+
+def test_suggestions_command():
+    result = runner.invoke(cli, ["suggestions", "-k", "sun"])
+    assert "phrase" in result.output
+
+
+def test_translate_command():
+    result = runner.invoke(cli, ["translate", "-k", "moon", "-t", "de"])
+    assert "language" in result.output
+
+
+def test_save_csv():
+    keywords = "butterfly"
+    with DDGS() as ddgs:
+        ddgs_gen = ddgs.text(keywords)
+        results = [x for x in islice(ddgs_gen, 25)]
+        assert len(results) >= 22
+
+    save_csv(f"{keywords}.csv", results)
+
+    # delete files and folders contains keyword in name
+    not_files = True
+    for name in os.listdir():
+        if keywords in name:
+            if os.path.isfile(name):
+                os.remove(name)
+                not_files = False
+    if not_files:
+        raise AssertionError("csv not found")
+
+
+def test_save_json():
+    keywords = "chicago"
+    with DDGS() as ddgs:
+        ddgs_gen = ddgs.text(keywords)
+        results = [x for x in islice(ddgs_gen, 25)]
+        assert len(results) >= 22
+
+    save_json(f"{keywords}.json", results)
+
+    # delete files and folders contains keyword in name
+    not_files = True
+    for name in os.listdir():
+        if keywords in name:
+            if os.path.isfile(name):
+                os.remove(name)
+                not_files = False
+    if not_files:
+        raise AssertionError("json not found")
+
+
+def test_text_download():
+    keywords = "maradona"
+    results = [x for x in islice(DDGS().text(keywords), 10)]
+    assert len(results) >= 8
+
+    download_results(keywords, results)
+
+    # delete files contains keyword in name
+    files = False
+    for dir in os.listdir("."):
+        if keywords in dir:
+            for filename in os.listdir(dir):
+                filename = f"{os.getcwd()}/{dir}/{filename}"
+                if os.path.isfile(filename):
+                    os.remove(filename)
+                    files = True
+    if not files:
+        raise AssertionError("images files not found")
+
+    # delete folder contains keyword in name
+    for dir in os.listdir():
+        if f"{keywords}" in dir:
+            if os.path.isdir(dir):
+                shutil.rmtree(dir)
+
+
+def test_images_download():
+    keywords = "real madrid"
+    results = [x for x in islice(DDGS().images(keywords), 10)]
+    assert len(results) >= 8
+
+    download_results(keywords, results, images=True)
+
+    # delete files contains keyword in name
+    files = False
+    for dir in os.listdir("."):
+        if keywords in dir:
+            for filename in os.listdir(dir):
+                filename = f"{os.getcwd()}/{dir}/{filename}"
+                if os.path.isfile(filename):
+                    os.remove(filename)
+                    files = True
+    if not files:
+        raise AssertionError("images files not found")
+
+    # delete folder contains keyword in name
+    for dir in os.listdir():
+        if f"{keywords}" in dir:
+            if os.path.isdir(dir):
+                shutil.rmtree(dir)
---
+++ tests/test_duckduckgo_search_async.py
@@ -0,0 +1,88 @@
+import pytest
+
+from duckduckgo_search import AsyncDDGS
+
+
+@pytest.mark.asyncio
+async def test_text():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.text("cat", max_results=25)]
+        assert len(results) >= 25
+
+
+@pytest.mark.asyncio
+async def test_text_params():
+    async with AsyncDDGS() as ddgs:
+        results = [
+            x
+            async for x in ddgs.text(
+                "cat", safesearch="off", timelimit="m", max_results=25
+            )
+        ]
+        assert len(results) >= 25
+
+
+@pytest.mark.asyncio
+async def test_text_html():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.text("eagle", backend="html", max_results=25)]
+        assert len(results) >= 25
+
+
+@pytest.mark.asyncio
+async def test_text_lite():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.text("dog", backend="lite", max_results=23)]
+        assert len(results) >= 23
+
+
+@pytest.mark.asyncio
+async def test_images():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.images("airplane", max_results=140)]
+        assert len(results) >= 140
+
+
+@pytest.mark.asyncio
+async def test_videos():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.videos("sea", max_results=40)]
+        assert len(results) >= 40
+
+
+@pytest.mark.asyncio
+async def test_news():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.news("tesla", max_results=27)]
+        assert len(results) >= 27
+
+
+@pytest.mark.asyncio
+async def test_maps():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.maps("school", place="London", max_results=40)]
+        assert len(results) >= 40
+
+
+@pytest.mark.asyncio
+async def test_answers():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.answers("sun")]
+        assert len(results) >= 1
+
+
+@pytest.mark.asyncio
+async def test_suggestions():
+    async with AsyncDDGS() as ddgs:
+        results = [x async for x in ddgs.suggestions("moon")]
+        assert len(results) >= 1
+
+
+@pytest.mark.asyncio
+async def test_translate():
+    results = await AsyncDDGS().translate("school", to="de")
+    assert results == {
+        "detected_language": "en",
+        "translated": "Schule",
+        "original": "school",
+    }
