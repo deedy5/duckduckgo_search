0f648d9fe17adbf333479fdec96caf8be3e677ec
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-02-27 18:34:10 +0300

    tests: recode to use DDGS without context manager

--- tests/test_duckduckgo_search.py
+++ tests/test_duckduckgo_search.py
@@ -9,79 +9,74 @@ def pause_between_tests():
     time.sleep(1)
 
 
-def test_text():
+def test_context_manager():
     with DDGS() as ddgs:
-        results = ddgs.text("cat", max_results=30)
+        results = ddgs.news("cars", max_results=30)
         assert len(results) == 30
+
+
+def test_text():
+    results = DDGS().text("cat", max_results=30)
+    assert len(results) == 30
 
 
 def test_text_params():
-    with DDGS() as ddgs:
-        results = ddgs.text("cat", safesearch="off", timelimit="m", max_results=30)
-        assert len(results) == 30
+    results = DDGS().text("cat", safesearch="off", timelimit="m", max_results=30)
+    assert len(results) == 30
 
 
 def test_text_html():
-    with DDGS() as ddgs:
-        results = ddgs.text("eagle", backend="html", max_results=30)
-        assert len(results) == 30
+    results = DDGS().text("eagle", backend="html", max_results=30)
+    assert len(results) == 30
 
 
 def test_text_lite():
-    with DDGS() as ddgs:
-        results = ddgs.text("dog", backend="lite", max_results=30)
-        assert len(results) == 30
+    results = DDGS().text("dog", backend="lite", max_results=30)
+    assert len(results) == 30
 
 
 def test_images():
-    with DDGS() as ddgs:
-        results = ddgs.images("airplane", max_results=140)
-        assert len(results) == 140
+    results = DDGS().images("airplane", max_results=140)
+    assert len(results) == 140
 
 
 def test_videos():
-    with DDGS() as ddgs:
-        results = ddgs.videos("sea", max_results=40)
-        assert len(results) == 40
+    results = DDGS().videos("sea", max_results=40)
+    assert len(results) == 40
 
 
 def test_news():
-    with DDGS() as ddgs:
-        results = ddgs.news("tesla", max_results=30)
-        assert len(results) == 30
+    results = DDGS().news("tesla", max_results=30)
+    assert len(results) == 30
 
 
 def test_maps():
-    with DDGS() as ddgs:
-        results = ddgs.maps("school", place="London", max_results=30)
-        assert len(results) == 30
+    results = DDGS().maps("school", place="London", max_results=30)
+    assert len(results) == 30
 
 
 def test_answers():
-    with DDGS() as ddgs:
-        results = ddgs.answers("sun")
-        assert len(results) >= 1
+    results = DDGS().answers("sun")
+    assert len(results) >= 1
 
 
 def test_suggestions():
-    with DDGS() as ddgs:
-        results = ddgs.suggestions("moon")
-        assert len(results) >= 1
+    results = DDGS().suggestions("moon")
+    assert len(results) >= 1
 
 
 def test_translate():
-    with DDGS() as ddgs:
-        results = ddgs.translate(["school", "tomatoes"], to="de")
-        expected_results = [
-            {
-                "detected_language": "en",
-                "translated": "Schule",
-                "original": "school",
-            },
-            {
-                "detected_language": "en",
-                "translated": "Tomaten",
-                "original": "tomatoes",
-            }
-        ]
-        assert all(er in results for er in expected_results)
+    results = DDGS().translate(["school", "tomatoes"], to="de")
+    expected_results = [
+        {
+            "detected_language": "en",
+            "translated": "Schule",
+            "original": "school",
+        },
+        {
+            "detected_language": "en",
+            "translated": "Tomaten",
+            "original": "tomatoes",
+        }
+    ]
+    assert all(er in results for er in expected_results)
