0ba53d3e86586f56b5e76d4034d8f240e3092b90
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-03-01 17:17:08 +0300

    Dependencies: update ruff to 0.3.0 and format files

--- duckduckgo_search/__init__.py
+++ duckduckgo_search/__init__.py
@@ -1,6 +1,6 @@
 """Duckduckgo_search.
 
-Search for words, documents, images, videos, news, maps and text translation 
+Search for words, documents, images, videos, news, maps and text translation
 using the DuckDuckGo.com search engine.
 """
 
--- duckduckgo_search/__main__.py
+++ duckduckgo_search/__main__.py
@@ -1,4 +1,5 @@
 """For using as 'python3 -m duckduckgo_search'."""
+
 from .cli import cli
 
 if __name__ == "__main__":
