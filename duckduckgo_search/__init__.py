dbc7a6d97094cfb414cae8e32f9b5e3fc2618d44
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-04-06 18:46:36 +0300

    __init__(): update comments

--- duckduckgo_search/__init__.py
+++ duckduckgo_search/__init__.py
@@ -14,7 +14,8 @@ from .version import __version__
 
 __all__ = ["DDGS", "AsyncDDGS", "__version__", "cli"]
 
-# bypass curl_cffi warning on windows: https://github.com/yifeikong/curl_cffi/blob/418e452c99dee5da176f0b0a768337cd5509c4c5/curl_cffi/aio.py#L14
+
+# bypass Windows error "RuntimeError: Event Loop is closed" on exit. https://github.com/encode/httpx/issues/914
 if sys.platform == "win32":
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
