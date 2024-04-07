9c0d6026f3dd8d9b9b2eb3b017d88ca0d058ce36
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-04-07 13:13:46 +0300

    update tests

--- tests/test_duckduckgo_search.py
+++ tests/test_duckduckgo_search.py
@@ -37,7 +37,7 @@ def test_text_lite():
 
 def test_images():
     results = DDGS().images("flower", max_results=200)
-    assert 100 <= len(results) <= 200
+    assert 95 <= len(results) <= 200
 
 
 def test_videos():
