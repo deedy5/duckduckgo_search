740634d0175d7b75d105fa5f91cb59a51906538a
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-07-27 11:35:47 +0300

    `impersonate`: add `edge_127`, remove `chrome_99`

--- duckduckgo_search/duckduckgo_search.py
+++ duckduckgo_search/duckduckgo_search.py
@@ -39,13 +39,13 @@ class DDGS:
 
     _executor: ThreadPoolExecutor = ThreadPoolExecutor()
     _impersonates = (
-        "chrome_99", "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_108", 
-        "chrome_107", "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118", "chrome_119", 
-        "chrome_120", #"chrome_123", "chrome_124", "chrome_126",
+        "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_107", "chrome_108", 
+        "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118", "chrome_119", "chrome_120", 
+        #"chrome_123", "chrome_124", "chrome_126",
         "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1", "safari_15.3", "safari_15.5", 
         "safari_15.6.1", "safari_16", "safari_16.5", "safari_17.2.1", "safari_17.4.1", "safari_17.5",
         #"okhttp_3.9", "okhttp_3.11", "okhttp_3.13", "okhttp_3.14", "okhttp_4.9", "okhttp_4.10", "okhttp_5",
-        "edge_99", "edge_101", "edge_122",
+        "edge_99", "edge_101", "edge_122", "edge_127",
     )  # fmt: skip
 
     def __init__(
