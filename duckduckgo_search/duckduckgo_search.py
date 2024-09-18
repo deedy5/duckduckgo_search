71e9214a683526f787e5aa7cbebfab11e2651d03
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-09-18 22:59:16 +0300

    DDGS._impersonates: add `chrome_128`

--- duckduckgo_search/duckduckgo_search.py
+++ duckduckgo_search/duckduckgo_search.py
@@ -45,7 +45,7 @@ class DDGS:
         "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_107", "chrome_108", 
         "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118", "chrome_119", "chrome_120", 
         #"chrome_123", "chrome_124", "chrome_126",
-        "chrome_127",
+        "chrome_127", "chrome_128",
         "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1", "safari_15.3", "safari_15.5", "safari_15.6.1", 
         "safari_16", "safari_16.5", "safari_17.0", "safari_17.2.1", "safari_17.4.1", "safari_17.5",
         "edge_101", "edge_122", "edge_127",
