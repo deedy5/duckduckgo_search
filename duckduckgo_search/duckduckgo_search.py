6ea164b4847a72ce314709373ed4d43a813f1542
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-12-21 08:48:37 +0300

    DDGS._impersonates: add "safari_ios_18.1.1", "safari_18.2", "edge_131"

--- duckduckgo_search/duckduckgo_search.py
+++ duckduckgo_search/duckduckgo_search.py
@@ -47,11 +47,12 @@ class DDGS:
         "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118", "chrome_119", "chrome_120",
         "chrome_123", "chrome_124", "chrome_126", "chrome_127", "chrome_128", "chrome_129", "chrome_130",
         "chrome_131",
-        "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1",
+        "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1", "safari_ios_18.1.1",
         "safari_15.3", "safari_15.5", "safari_15.6.1", "safari_16", "safari_16.5",
-        "safari_17.0", "safari_17.2.1", "safari_17.4.1", "safari_17.5", "safari_18",
+        "safari_17.0", "safari_17.2.1", "safari_17.4.1", "safari_17.5",
+        "safari_18", "safari_18.2",
         "safari_ipad_18",
-        "edge_101", "edge_122", "edge_127",
+        "edge_101", "edge_122", "edge_127", "edge_131",
     )  # fmt: skip
 
     def __init__(
