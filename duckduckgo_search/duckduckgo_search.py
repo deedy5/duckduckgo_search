3f752c93be1978e70ce61f4309eba16f1753a051
deedy5 <65482418+deedy5@users.noreply.github.com>
2024-12-23 20:30:58 +0300

    DDGS._impersonates: add "okhttp_3.13", "okhttp_3.14", "okhttp_4.9", "okhttp_4.10", "okhttp_5", "firefox_109", "firefox_133",

--- duckduckgo_search/duckduckgo_search.py
+++ duckduckgo_search/duckduckgo_search.py
@@ -33,16 +33,18 @@ class DDGS:
     """DuckDuckgo_search class to get search results from duckduckgo.com."""
 
     _impersonates = (
-        "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_107", "chrome_108",
-        "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118", "chrome_119", "chrome_120",
-        "chrome_123", "chrome_124", "chrome_126", "chrome_127", "chrome_128", "chrome_129", "chrome_130",
-        "chrome_131",
+        "chrome_100", "chrome_101", "chrome_104", "chrome_105", "chrome_106", "chrome_107",
+        "chrome_108", "chrome_109", "chrome_114", "chrome_116", "chrome_117", "chrome_118",
+        "chrome_119", "chrome_120", "chrome_123", "chrome_124", "chrome_126", "chrome_127",
+        "chrome_128", "chrome_129", "chrome_130", "chrome_131",
         "safari_ios_16.5", "safari_ios_17.2", "safari_ios_17.4.1", "safari_ios_18.1.1",
         "safari_15.3", "safari_15.5", "safari_15.6.1", "safari_16", "safari_16.5",
         "safari_17.0", "safari_17.2.1", "safari_17.4.1", "safari_17.5",
         "safari_18", "safari_18.2",
         "safari_ipad_18",
         "edge_101", "edge_122", "edge_127", "edge_131",
+        "okhttp_3.13", "okhttp_3.14", "okhttp_4.9", "okhttp_4.10", "okhttp_5",
+        "firefox_109", "firefox_133",
     )  # fmt: skip
 
     def __init__(
