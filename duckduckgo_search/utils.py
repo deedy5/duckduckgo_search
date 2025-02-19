1b53665e7267e6d4a5492e696f6191808d40ba66
deedy5 <65482418+deedy5@users.noreply.github.com>
2025-02-19 16:33:48 +0300

    feat: patch httpcore

--- duckduckgo_search/utils.py
+++ duckduckgo_search/utils.py
@@ -5,11 +5,13 @@ import re
 import ssl
 from html import unescape
 from pathlib import Path
-from random import SystemRandom, choice, choices
+from random import SystemRandom, choice, choices, randint
 from typing import Any
 from urllib.parse import unquote
 
 import certifi
+import h2
+import httpcore
 
 from .exceptions import DuckDuckGoSearchException
 
@@ -19,51 +21,8 @@ try:
 except ImportError:
     HAS_ORJSON = False
     import json
+
 REGEX_STRIP_TAGS = re.compile("<.*?>")
-
-
-CRYPTORAND = SystemRandom()
-SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
-# Include all cipher suites that Cloudflare supports today. https://developers.cloudflare.com/ssl/reference/cipher-suites/recommendations/
-
-DEFAULT_CIPHERS = [
-    # Modern:
-    "ECDHE-ECDSA-AES128-GCM-SHA256",
-    "ECDHE-ECDSA-CHACHA20-POLY1305",
-    "ECDHE-RSA-AES128-GCM-SHA256",
-    "ECDHE-RSA-CHACHA20-POLY1305",
-    "ECDHE-ECDSA-AES256-GCM-SHA384",
-    "ECDHE-RSA-AES256-GCM-SHA384",
-    # Compatible:
-    "ECDHE-ECDSA-AES128-GCM-SHA256",
-    "ECDHE-ECDSA-CHACHA20-POLY1305",
-    "ECDHE-RSA-AES128-GCM-SHA256",
-    "ECDHE-RSA-CHACHA20-POLY1305",
-    "ECDHE-ECDSA-AES256-GCM-SHA384",
-    "ECDHE-RSA-AES256-GCM-SHA384",
-    "ECDHE-ECDSA-AES128-SHA256",
-    "ECDHE-RSA-AES128-SHA256",
-    "ECDHE-ECDSA-AES256-SHA384",
-    "ECDHE-RSA-AES256-SHA384",
-    # Legacy:
-    "ECDHE-ECDSA-AES128-SHA",
-    "ECDHE-RSA-AES128-SHA",
-    "AES128-GCM-SHA256",
-    "AES128-SHA256",
-    "AES128-SHA",
-    "ECDHE-RSA-AES256-SHA",
-    "AES256-GCM-SHA384",
-    "AES256-SHA256",
-    "AES256-SHA",
-    "DES-CBC3-SHA",
-]
-
-
-def _get_random_ssl_context() -> ssl.SSLContext:
-    """Get SSL context with shuffled ciphers."""
-    shuffled_ciphers = CRYPTORAND.sample(DEFAULT_CIPHERS[6:], len(DEFAULT_CIPHERS) - 6)
-    SSL_CONTEXT.set_ciphers(":".join(DEFAULT_CIPHERS[:6] + shuffled_ciphers))
-    return SSL_CONTEXT
 
 
 def json_dumps(obj: Any) -> str:
@@ -115,7 +74,31 @@ def _expand_proxy_tb_alias(proxy: str | None) -> str | None:
     return "socks5://127.0.0.1:9150" if proxy == "tb" else proxy
 
 
-# Load headers from file headers.json.gz
+# SSL
+CRYPTORAND = SystemRandom()
+SSL_CONTEXT = ssl.create_default_context(cafile=certifi.where())
+DEFAULT_CIPHERS = [  # https://developers.cloudflare.com/ssl/reference/cipher-suites/recommendations/
+    # Modern:
+    "ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-ECDSA-CHACHA20-POLY1305", "ECDHE-RSA-AES128-GCM-SHA256",
+    "ECDHE-RSA-CHACHA20-POLY1305", "ECDHE-ECDSA-AES256-GCM-SHA384", "ECDHE-RSA-AES256-GCM-SHA384",
+    # Compatible:
+    "ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-ECDSA-CHACHA20-POLY1305", "ECDHE-RSA-AES128-GCM-SHA256",
+    "ECDHE-RSA-CHACHA20-POLY1305", "ECDHE-ECDSA-AES256-GCM-SHA384", "ECDHE-RSA-AES256-GCM-SHA384",
+    "ECDHE-ECDSA-AES128-SHA256", "ECDHE-RSA-AES128-SHA256", "ECDHE-ECDSA-AES256-SHA384",  "ECDHE-RSA-AES256-SHA384",
+    # Legacy:
+    "ECDHE-ECDSA-AES128-SHA", "ECDHE-RSA-AES128-SHA", "AES128-GCM-SHA256", "AES128-SHA256", "AES128-SHA",
+    "ECDHE-RSA-AES256-SHA", "AES256-GCM-SHA384", "AES256-SHA256", "AES256-SHA", "DES-CBC3-SHA",
+]  # fmt: skip
+
+
+def _get_random_ssl_context() -> ssl.SSLContext:
+    """Get SSL context with shuffled ciphers."""
+    shuffled_ciphers = CRYPTORAND.sample(DEFAULT_CIPHERS[6:], len(DEFAULT_CIPHERS) - 6)
+    SSL_CONTEXT.set_ciphers(":".join(DEFAULT_CIPHERS[:6] + shuffled_ciphers))
+    return SSL_CONTEXT
+
+
+# Headers
 script_dir = Path(__file__).parent
 file_path = script_dir / "headers.json.gz"
 with gzip.open(file_path, "rt") as f:
@@ -132,3 +115,25 @@ def _get_probability_headers() -> dict[str, str]:
 def _get_random_headers() -> dict[str, str]:
     """Get random headers."""
     return choice(HEADERS)
+
+
+# Patch httpcore
+def _send_connection_init(self, request):  # type: ignore
+    self._h2_state.local_settings = h2.settings.Settings(
+        client=True,
+        initial_values={
+            h2.settings.SettingCodes.INITIAL_WINDOW_SIZE: randint(100, 200),
+            h2.settings.SettingCodes.HEADER_TABLE_SIZE: randint(4000, 5000),
+            h2.settings.SettingCodes.MAX_FRAME_SIZE: randint(16384, 17384),
+            h2.settings.SettingCodes.MAX_CONCURRENT_STREAMS: randint(100, 200),
+            h2.settings.SettingCodes.MAX_HEADER_LIST_SIZE: randint(65500, 66500),
+            h2.settings.SettingCodes.ENABLE_CONNECT_PROTOCOL: randint(0, 1),
+            h2.settings.SettingCodes.ENABLE_PUSH: randint(0, 1),
+        },
+    )
+    self._h2_state.initiate_connection()
+    self._h2_state.increment_flow_control_window(2**24)
+    self._write_outgoing_data(request)
+
+
+httpcore._sync.http2.HTTP2Connection._send_connection_init = _send_connection_init  # type: ignore
